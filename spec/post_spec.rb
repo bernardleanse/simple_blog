2ed6377dfcc1901935d68998086bfb127fa2359a
Bernard Leanse <bernardleanse96@gmail.com>
2022-02-19 14:40:10 +0000

    test for returning all posts

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -8,4 +8,13 @@ describe Post do
       expect(saved_post['content']).to eq("my test post")
     end
   end
+
+  describe ".all" do
+    it "returns all posts" do
+      Post.create(content: 'hello')
+      Post.create(content: 'goodbye')
+      expect(Post.all.first.content).to eq 'hello'
+      expect(Post.all.last.content).to eq 'goodbye'
+    end
+  end
 end
