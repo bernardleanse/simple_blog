5f4d814d977a84a4089544a9300c3dccfd2c6729
Bernard Leanse <bernardleanse96@gmail.com>
2022-02-19 12:47:45 +0000

    sinatra and rackup setup

---
+++ app.rb
@@ -0,0 +1,17 @@
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class BlogSite < Sinatra::Base
+  enable :sessions, :method_override
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Welcome to the Blog Site!!'
+  end
+
+  run! if app_file == $0
+end
