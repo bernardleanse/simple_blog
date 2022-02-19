b5e8f97bf0422294e10a39a3fab13aeb56bf1fbc
Bernard Leanse <bernardleanse96@gmail.com>
2022-02-19 14:45:53 +0000

    post returns all posts and wraps in post instance

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,16 @@
 class Post
+
+  def self.all
+    posts = DatabaseConnection.query("SELECT * FROM posts;")
+    posts.map { |post| Post.new(id: post['id'], content: post['content']) }
+  end
+
   def self.create(content:)
     post = DatabaseConnection.query("INSERT INTO posts(content) VALUES($1) RETURNING id, content;", [content]).first
     return Post.new(id: post["id"], content: post["content"])
   end
+
+  attr_reader :id, :content
 
   def initialize(id:, content:)
     @id = id
