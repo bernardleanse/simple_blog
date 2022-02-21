40a3510c406505d11fcd38fe6c225d5d346456d8
Bernard Leanse <bernardleanse96@gmail.com>
2022-02-21 12:29:27 +0000

    unit test for creating a User

---
+++ spec/unit/user_spec.rb
@@ -0,0 +1,14 @@
+require 'user'
+
+describe User do
+  describe ".create" do
+    it 'creates a user, password match' do
+      user = User.create(username: 'bernard', password: '123', confirm_password: '123')
+      db_data = DatabaseConnection.query("SELECT * FROM users WHERE username=$1;", ['bernard'])
+
+      expect(user).to be_a User
+      expect(user.id).to eq db_data.first['id']
+      expect(user.username).to eq db_data.first['username']
+    end
+  end
+end
