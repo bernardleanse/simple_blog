d75174869b31e1d1139eba32a7f6e88c0c10f516
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-03 16:00:17 +0000

    refactor into partial

--- spec/features/delete_post_spec.rb
+++ spec/features/delete_post_spec.rb
@@ -1,6 +1,7 @@
 feature 'deleting a post' do
   scenario 'user makes a post then deletes it' do
     post = Post.create(content: 'my post')
+    allow(:session).to receiv
     visit '/posts'
     expect(page).to have_content 'my post'
     first('.post').click_button('Delete')
--- spec/features/only_edit_own_posts_spec.rb
+++ spec/features/only_edit_own_posts_spec.rb
@@ -1,7 +1,12 @@
-feature 'only edit own posts' do
-  scenario 'not logged in, tries to edit post' do
+feature 'only see edit, delete, and comment if logged in' do
+  scenario 'not logged in' do
     user = User.create(username: 'test', password: 'oi', confirm_password: 'oi')
     Post.create(content: 'test post', user_id: user.id)
-    
+    # no session yet
+    visit '/posts'
+    expect(first '.post').not_to have_button 'Edit'
+    expect(first '.post').not_to have_button 'Delete'
+    expect(first '.post').not_to have_button 'Comment'
+
   end
 end
