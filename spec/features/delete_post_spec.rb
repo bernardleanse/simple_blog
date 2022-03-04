57ab8d1c20c4c2061b90e6ffeb4607c874bc23d2
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-04 11:11:57 +0000

    deleteing posts fixed

--- spec/features/delete_post_spec.rb
+++ spec/features/delete_post_spec.rb
@@ -1,11 +1,10 @@
 feature 'deleting a post' do
   scenario 'user makes a post then deletes it' do
-    post = Post.create(content: 'my post')
-    visit '/posts'
-    user = User.create()
-    expect(page).to have_content 'my post'
+    sign_up_and_log_in
+    create_test_post
+    expect(page).to have_content 'test'
     first('.post').click_button('Delete')
     expect(current_path).to eq '/posts'
-    expect(page).not_to have_content 'my post'
+    expect(page).not_to have_content 'test'
   end
 end
