15b8ff8ff7c63b5293f939f2ae284a9e47720289
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-04 11:17:14 +0000

    all tests now fixed, user can log out

--- spec/features/user_log_out_spec.rb
+++ spec/features/user_log_out_spec.rb
@@ -1,10 +1,9 @@
 feature 'log out' do
-  xscenario 'a logged in user can log out' do
-    User.create(username: 'bernard', password: '123', confirm_password: '123')
+  scenario 'a logged in user can log out' do
     sign_up_and_log_in
-    expect(page).to have_content 'bernard'
+    expect(page).to have_content 'baz'
     click_button 'Log Out'
-    expect(page).not_to have_content 'bernard'
+    expect(page).not_to have_content 'baz'
     expect(current_path).to eq '/posts'
     expect(page).to have_content 'Successfully Logged Out'
   end
