933d0b71e160a0c55fa2a33ddc3d0e6ab4cfac1f
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-04 11:46:27 +0000

    test for trying to edit by url when not logged in

--- spec/features/only_edit_own_posts_spec.rb
+++ spec/features/only_edit_own_posts_spec.rb
@@ -7,6 +7,14 @@ feature 'only see edit, delete, and comment if logged in' do
     expect(first '.post').not_to have_button 'Edit'
     expect(first '.post').not_to have_button 'Delete'
     expect(first '.post').not_to have_button 'Comment'
+  end
 
+  scenario 'tries to bypass edit' do
+    sign_up_and_log_in
+    post = create_test_post
+    click_button 'Log Out'
+    visit "posts/#{post['id']}/edit"
+    expect(current_path).to eq '/posts'
+    expect(page).to have_content "You don't have permission"
   end
 end
